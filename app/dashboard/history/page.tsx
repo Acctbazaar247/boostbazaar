739b47823712d206b7a717bd0a610074d96ff7dd
naimur53 <naimurrhman53@gmail.com>
2025-02-07 16:45:41 +0600

    eo

--- app/dashboard/history/page.tsx
+++ app/dashboard/history/page.tsx
@@ -254,9 +254,8 @@ const Page = () => {
           <AppInfo>
             <p>
               SMS services codes are processed within 0-30 seconds, but some
-              might take up to a minute depending on platform. If you
-              don`&apos;t get you code click &lsquo;Refund&lsquo; and restart
-              the process
+              might take up to a minute depending on platform. If you don&apos;t
+              get you code click &lsquo;Refund&lsquo; and restart the process
             </p>
           </AppInfo>
         </div>
